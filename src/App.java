9e031378bf05370cf8ffc87dc90829bc34097a22
Jason D'Oyley <jdfdoyley@gmail.com>
2023-01-12 18:28:55 -0500

    Demo how to use a simple exception

--- src/App.java
+++ src/App.java
@@ -5,4 +5,19 @@
  */
 
 public class App {
+    public static void main(String[] args) {
+        int [] nums = new int[4];
+
+        try {
+            System.out.println("Before exception is generated");
+
+            //     Generate an index out-of-bound exception
+            nums[0] = 10;
+            System.out.println("this won't be displayed");
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            //     catch the exception
+            System.out.println("Index out-of-bounds!");
+        }
+        System.out.println("After catch statement.");
+    }
 }
